89a877e2c84bbec73adad739656cedfcedb061a3
Dejan Paunov <dpaunov21@yahoo.com>
2018-05-04 21:17:10 +0200

    Initial

---
+++ PuzzleBubble/Bubble.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleBubble {
+    public class Bubble {
+        public static int Radius = 25;
+        public Point Center { get; set; }
+        public Color color { get; set; }
+        public Point direction { get; set; }
+
+        public Bubble(int x,int y,Color color,Point start) {
+            this.Center = new Point(x, y);
+            this.color = color;
+            direction = start;
+        }
+
+
+        public void Draw(Graphics g) {
+            Brush b = new SolidBrush(color);
+            g.FillEllipse(b, Center.X - Radius, Center.Y - Radius, Radius * 2, Radius * 2);
+        }
+
+        public void DrawDirection(Graphics g,Point start) {
+            Pen p = new Pen(new SolidBrush(color));
+            g.DrawLine(p, start, direction);
+            
+
+        }
+
+
+        public void Move() {
+
+            Center = new Point(Center.X, Center.Y - 30);
+        }
+        public void DirectionLeft() {
+
+            direction = new Point(direction.X - 20, direction.Y);
+
+        }
+
+        public void DirectionRight() {
+
+            direction = new Point(direction.X + 20, direction.Y);
+
+        }
+
+
+    }
+}
---
+++ PuzzleBubble/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleBubble {
+    public partial class Form1 : Form {
+        public Point start;
+        public Color bubbleColor;
+        public Bubble bubble;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            start = new Point(panel1.Width / 2, panel1.Height - 2 * Bubble.Radius);
+            bubbleColor = Color.Red;
+            bubble = new Bubble(start.X, start.Y, Color.Red, new Point(Width/2,0));
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) { 
+                 bubble.Draw(e.Graphics);
+                 bubble.DrawDirection(e.Graphics, start);
+        }
+
+        private void panel1_Resize(object sender, EventArgs e) {
+            Invalidate(true);
+        }
+
+        private void panel1_DockChanged(object sender, EventArgs e) {
+            Invalidate();
+        }
+
+        private void panel1_SizeChanged(object sender, EventArgs e) {
+            Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Left) {
+                bubble.DirectionLeft();
+            }
+            if (e.KeyCode == Keys.Left) {
+                bubble.DirectionRight();
+            }
+
+
+            Invalidate(true);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e) {
+            bubble.DirectionRight();
+            Invalidate();
+        }
+    }
+}
